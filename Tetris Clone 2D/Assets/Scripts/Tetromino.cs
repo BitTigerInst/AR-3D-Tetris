6efa76f522d39a6370700f2b9ca544733f96021d
cctv2206 <cctv2206@outlook.com>
2016-07-30 16:03:49 -0500

    2D Tetris game test
    Created the Grid and all the Tetrominos. Basic falling and user input
    and limits

---
+++ Tetris Clone 2D/Assets/Scripts/Tetromino.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tetromino : MonoBehaviour {
+
+	float fall = 0;
+	public float fallSpeed = 1;
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CheckUserInput ();
+
+	
+	}
+
+	void CheckUserInput () {
+		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+
+			transform.position += new Vector3 (-1, 0, 0);
+
+			if (CheckIsValidPosition ()) {
+				
+			} else {
+				transform.position += new Vector3 (1, 0, 0);
+			}
+
+		} else if (Input.GetKeyDown (KeyCode.RightArrow)) {
+
+			transform.position += new Vector3 (1, 0, 0);
+
+			if (CheckIsValidPosition ()) {
+
+			} else {
+				transform.position += new Vector3 (-1, 0, 0);
+			}
+			
+		} else if (Input.GetKeyDown (KeyCode.UpArrow)) {
+
+			transform.Rotate (0, 0, 90);
+
+			if (CheckIsValidPosition ()) {
+
+			} else {
+				transform.Rotate (0, 0, -90);
+			}
+			
+		} else if (Input.GetKeyDown (KeyCode.DownArrow) || Time.time - fall >= fallSpeed) {
+
+			transform.position += new Vector3 (0, -1, 0);
+
+			if (CheckIsValidPosition ()) {
+
+			} else {
+				transform.position += new Vector3 (0, 1, 0);
+			}
+
+			fall = Time.time;
+			
+		}
+
+	}
+
+	bool CheckIsValidPosition() {
+
+		foreach (Transform mino in transform) {
+
+			Vector2 pos = FindObjectOfType<Game> ().Round (mino.position);
+
+			if (FindObjectOfType<Game> ().CheckIsInsideGrid (pos) == false) {
+				return false;
+			}
+		}
+
+		return true;
+
+	}
+
+}
