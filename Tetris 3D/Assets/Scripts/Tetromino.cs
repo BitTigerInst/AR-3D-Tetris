f09fa035cf7a8b84a04429ba535215dcad1ed1c1
cctv2206 <cctv2206@outlook.com>
2016-08-08 10:57:29 -0500

    3D Tetrominos and Grid

--- Tetris 3D/Assets/Scripts/Tetromino.cs
+++ Tetris 3D/Assets/Scripts/Tetromino.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class Tetromino : MonoBehaviour {
 
+	float fall = 0;
+	public float fallSpeed = 1;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +13,35 @@ public class Tetromino : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+		CheckUserInput ();
 	
+	}
+
+
+	void CheckUserInput () {
+		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+
+			transform.position += new Vector3 (-1, 0, 0);
+
+
+		} else if (Input.GetKeyDown (KeyCode.RightArrow)) {
+
+			transform.position += new Vector3 (1, 0, 0);
+		
+
+		} else if (Input.GetKeyDown (KeyCode.UpArrow)) {
+
+
+
+		} else if (Input.GetKeyDown (KeyCode.DownArrow) || Time.time - fall >= fallSpeed) {
+
+			transform.position += new Vector3 (0, -1, 0);
+		
+
+			fall = Time.time;
+
+		}
+
 	}
 }
